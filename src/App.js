c885bd7ca78f15032d8ec064d0fc2284761af2b5
[AlyarQuliyev] <[quliyev20@gmail.com]>
2022-12-22 15:15:12 +0400

    dice-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Dice from "./components/Dice";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    window.addEventListener("click", this.spaceBarPressed);
+  }
+  spaceBarPressed = (e) => {
+    if (e.target.tagName.toLowerCase() === "html") {
+      this.refs.child.shuffleDices();
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="dices">
+          <Dice ref="child" key="1" />
+          <Dice ref="child" key="2" />
+          <Dice ref="child" key="3" />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
